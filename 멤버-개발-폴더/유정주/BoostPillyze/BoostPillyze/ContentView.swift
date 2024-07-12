e937564ac1ed8125a01101a6bb58b00a11ba361d
유정주 <dbwjdwn9216@gmail.com>
2024-07-12 23:22:07 +0900

    [정주] 프로젝트 생성

---
+++ 멤버-개발-폴더/유정주/BoostPillyze/BoostPillyze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoostPillyze
+//
+//  Created by 유정주 on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
