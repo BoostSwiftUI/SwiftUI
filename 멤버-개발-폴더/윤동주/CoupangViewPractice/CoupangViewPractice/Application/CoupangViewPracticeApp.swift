8385a4dd924002ae64589c9494387359258a65bd
Xerath <yoondj98@naver.com>
2024-07-12 18:21:56 +0900

    [동주] init project

---
+++ 멤버-개발-폴더/윤동주/CoupangViewPractice/CoupangViewPractice/Application/CoupangViewPracticeApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoupangViewPracticeApp.swift
+//  CoupangViewPractice
+//
+//  Created by 윤동주 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CoupangViewPracticeApp: App {
+    @State private var modelData = ModelData()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(modelData)
+                .environment(\.locale, .init(identifier: "ko"))
+        }
+    }
+}
