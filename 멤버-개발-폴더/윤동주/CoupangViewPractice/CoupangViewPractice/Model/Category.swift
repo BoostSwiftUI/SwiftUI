dd7556424b45ad8b3861ff61eb1228fdfb7f0dd3
Xerath <yoondj98@naver.com>
2024-07-12 18:23:19 +0900

    [동주] Model 구현 및 JSON 파일 load 연동

---
+++ 멤버-개발-폴더/윤동주/CoupangViewPractice/CoupangViewPractice/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  CoupangViewPractice
+//
+//  Created by 윤동주 on 7/8/24.
+//
+
+import Foundation
+
+struct Category: Hashable, Codable {
+    let categoryName: String
+    let categoryIcon: String
+}
---
+++ 멤버-개발-폴더/윤동주/CoupangViewPractice/CoupangViewPractice/Model/Product.swift
@@ -0,0 +1,34 @@
+//
+//  Product.swift
+//  CoupangViewPractice
+//
+//  Created by 윤동주 on 7/8/24.
+//
+
+import Foundation
+
+struct Product: Hashable, Codable {
+    let productName: String
+    let categories: [String]
+    let imageUrl: String
+    let price: Int
+    let discountRate: Int
+    let rocketDelivery: Bool
+    let rocketWow: Bool
+    let rating: Double
+    let reviewCount: Int
+    let sellerName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case productName = "product_name"
+        case categories
+        case imageUrl = "image_url"
+        case price
+        case discountRate = "discount_rate"
+        case rocketDelivery = "rocket_delivery"
+        case rocketWow = "rocket_wow"
+        case rating
+        case reviewCount = "review_count"
+        case sellerName = "seller_name"
+    }
+}
