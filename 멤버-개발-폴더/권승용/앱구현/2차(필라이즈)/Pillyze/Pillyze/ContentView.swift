016cdd8edd80c83213bf0a1ff4270f8a90904eaa
ericKwon95 <ericyong95@gmail.com>
2024-07-12 16:13:29 +0900

    [승용] feat: 필라이즈 클론 프로젝트 생성

---
+++ 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pillyze
+//
+//  Created by 권승용 on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
