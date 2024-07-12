cd2345ff92be55b830fcce5f5b5a9d21a6d792d1
SeungHyun Hong <whi7ehyun@gmail.com>
2024-07-12 23:36:00 +0900

    add: Add initial project about pillyze

---
+++ 멤버-개발-폴더/홍승현/pillyze/pillyzeUITests/pillyzeUITests.swift
@@ -0,0 +1,14 @@
+//
+//  pillyzeUITests.swift
+//  pillyzeUITests
+//
+//  Created by 홍승현 on 7/12/24.
+//
+
+import XCTest
+
+final class pillyzeUITests: XCTestCase {
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+}
---
+++ 멤버-개발-폴더/홍승현/pillyze/pillyzeUITests/pillyzeUITestsLaunchTests.swift
@@ -0,0 +1,19 @@
+//
+//  pillyzeUITestsLaunchTests.swift
+//  pillyzeUITests
+//
+//  Created by 홍승현 on 7/12/24.
+//
+
+import XCTest
+
+final class pillyzeUITestsLaunchTests: XCTestCase {
+  
+  override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    true
+  }
+  
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+}
