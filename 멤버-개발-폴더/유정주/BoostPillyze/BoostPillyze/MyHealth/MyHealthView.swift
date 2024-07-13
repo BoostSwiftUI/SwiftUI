70590418f5dcd28a86693fca8b408bd29845f14d
유정주 <dbwjdwn9216@gmail.com>
2024-07-13 23:16:24 +0900

    [정주] 캘린더 리스트 표시

--- 멤버-개발-폴더/유정주/BoostPillyze/BoostPillyze/MyHealth/MyHealthView.swift
+++ 멤버-개발-폴더/유정주/BoostPillyze/BoostPillyze/MyHealth/MyHealthView.swift
@@ -33,11 +33,85 @@ struct MyHealthView: View {
                     }
                 }
                 .padding(.horizontal)
+                
+                CalendarList()
             }
         }
         .background(Color.primaryNormal)
     }
 }
+
+// MARK: - Calendar
+
+private struct CalendarList: View {
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<31) { index in
+                    CalendarListItem(
+                        isToday: index == 0,
+                        week: dates[index % 7],
+                        day: days[index]
+                    )
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    // MARK: - Attribute
+    
+    private let dates = ["월", "화", "수", "목", "금", "토", "일"]
+    private let days = Array(1...31)
+}
+
+private struct CalendarListItem: View {
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            if isToday {
+                Text(week)
+                    .font(.system(size: 12))
+                    .padding()
+                    .foregroundStyle(Color.primaryNormal)
+                    .background(
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 24, height: 24)
+                    )
+            } else {
+                Text(week)
+                    .font(.system(size: 12))
+                    .padding()
+                    .foregroundStyle(Color.primaryDisabled)
+            }
+            
+            Text("\(day)")
+                .foregroundStyle(.white)
+                .font(.system(size: 14, weight: .bold))
+        }
+    }
+    
+    // MARK: - Attribute
+    
+    private let isToday: Bool
+    private let week: String
+    private let day: Int
+    
+    // MARK: - Initializer
+    
+    init(isToday: Bool, week: String, day: Int) {
+        self.isToday = isToday
+        self.week = week
+        self.day = day
+    }
+}
+
 
 // MARK: - Preview
 
