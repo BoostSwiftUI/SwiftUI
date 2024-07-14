fc5248ce934a0dec3a97ac2649f4b8a13f28b0b5
유정주 <dbwjdwn9216@gmail.com>
2024-07-14 14:49:22 +0900

    [정주] SearchHeaderView 표시

--- 멤버-개발-폴더/유정주/BoostPillyze/BoostPillyze/AddDiet/AddDietView.swift
+++ 멤버-개발-폴더/유정주/BoostPillyze/BoostPillyze/AddDiet/AddDietView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct AddDietView: View {
+    
+    @State private var searchKeyword = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SearchHeaderView(searchKeyword: $searchKeyword)
+            .padding(.horizontal)
+        
+        Spacer()
     }
 }
+
+// MARK: - Search Header
+
+private struct SearchHeaderView: View {
+    
+    @Binding var searchKeyword: String
+    
+    var body: some View {
+        HStack {
+            HStack(spacing: 0) {
+                Image(systemName: "magnifyingglass")
+                    .padding(.leading, 16)
+                TextField("음식명으로 검색", text: $searchKeyword)
+                    .padding(.vertical)
+                    .padding(.horizontal, 4)
+            }
+            .background(Color.primaryPlaceholder)
+            .clipShape(Capsule())
+            
+            Button(
+                action: {},
+                label: {
+                    Text("취소")
+                        .font(.system(size: 15))
+                }
+            )
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+// MARK: - Preview
 
 #Preview {
     AddDietView()
