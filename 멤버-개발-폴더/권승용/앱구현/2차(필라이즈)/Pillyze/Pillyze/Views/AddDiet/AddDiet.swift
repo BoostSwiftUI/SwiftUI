b6e4220e7e1b6aa303aca7b7e9cbc4372a1f41ad
ericKwon95 <ericyong95@gmail.com>
2024-07-15 16:25:47 +0900

    [승용] feat: 식단 추가 화면 서치바 구현

---
+++ 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/AddDiet/AddDiet.swift
@@ -0,0 +1,32 @@
+//
+//  AddDiet.swift
+//  Pillyze
+//
+//  Created by 권승용 on 7/13/24.
+//
+
+import SwiftUI
+
+struct AddDiet: View {
+    var body: some View {
+        VStack {
+            HStack {
+                SearchBar()
+                Button {
+                    
+                } label: {
+                    Text("취소")
+                        .font(.system(size: 15))
+                        .fontWeight(.regular)
+                        .foregroundStyle(.textNormal)
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+#Preview {
+    AddDiet()
+}
