6ce5279dcf00b8fc0c6d6f8a6ba72f279fa605fb
ericKwon95 <ericyong95@gmail.com>
2024-07-15 21:53:35 +0900

    [승용] feat: 식단 리스트의 row 뷰 구현

---
+++ 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/AddDiet/DietList/DietList.swift
@@ -0,0 +1,18 @@
+//
+//  DietList.swift
+//  Pillyze
+//
+//  Created by 권승용 on 7/15/24.
+//
+
+import SwiftUI
+
+struct DietList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DietList()
+}
