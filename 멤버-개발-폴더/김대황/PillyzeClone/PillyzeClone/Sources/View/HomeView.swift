7371748bbfce470f6e4491ff1a97dda7b2c84ef5
qwerty3345 <qwerty3345@naver.com>
2024-07-16 21:00:08 +0900

    [ADD] 홈의 CheckScoreView 까지 구현

--- 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/View/HomeView.swift
+++ 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/View/HomeView.swift
@@ -11,25 +11,94 @@ struct HomeView: View {
     @State private var selectedDate: Date = .now
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                NavigationBar()
-                    .padding(.vertical, 8)
+        VStack(spacing: 0) {
+            NavigationBar()
+                .padding(.vertical, 8)
+                // .background(.primaryNormal)
 
-                WeeklyDatePicker(
-                    selectedDate: $selectedDate,
-                    startRangeDate: .now.beforeDate(dayBefore: 100),
-                    endRangeDate: .now
-                )
+            ScrollView {
+                VStack(spacing: 0) {
+                    weeklyDatePicker
 
-                Text("Hello, World!")
+                    content
+                }
             }
+            // .background(.primaryNormal)
         }
-        .background(.primaryNormal)
+        .background {
+            VStack(spacing: .zero) {
+                backgroundColors.top        // 상단 배경색
+                backgroundColors.bottom     // 하단 배경색
+            }
+            .ignoresSafeArea()
+        }
+
     }
 }
 
 // MARK: - Inner Views
+
+private extension HomeView {
+    var weeklyDatePicker: some View {
+        WeeklyDatePicker(
+            selectedDate: $selectedDate,
+            startRangeDate: .now.beforeDate(dayBefore: 100),
+            endRangeDate: .now
+        )
+        .padding(.bottom, 16)
+        .background(.primaryNormal)
+    }
+
+    var content: some View {
+        VStack(spacing: 16) {
+            checkScoreView
+
+            Text("Hello World")
+                .frame(height: 500)
+                .ignoresSafeArea()
+        }
+        .clipShape(
+            UnevenRoundedRectangle(
+                topLeadingRadius: 24,
+                topTrailingRadius: 24
+            )
+        )
+        .frame(maxHeight: .infinity)
+        .background(backgroundColors.bottom)
+    }
+
+    var checkScoreView: some View {
+        VStack(spacing: 0) {
+            Text("오늘 내 식사,\n몇 점 짜리 식단일까?")
+                .multilineTextAlignment(.center)
+                .font(.pretendard(.bold, size: 20))
+
+            Image(.myHealthScore)
+
+            Text("잘 먹어야 건강해요")
+                .foregroundStyle(.primaryNormal)
+                .padding(.vertical, 10)
+
+            Button("내 식단 점수 확인하기") {
+
+            }
+            .buttonStyle(.cta)
+        }
+        .padding(20)
+        .background(.white)
+        .clipShape(.rect(cornerRadius: 24))
+        .shadow(color: .primaryNormal.opacity(0.1), radius: 16)
+        .overlay(alignment: .topTrailing) {
+            Button {
+
+            } label: {
+                Image(.icClose)
+            }
+            .padding(16)
+        }
+        .padding(20)
+    }
+}
 
 fileprivate struct NavigationBar: View {
     var body: some View {
@@ -64,6 +133,14 @@ fileprivate struct NavigationBar: View {
     }
 }
 
+
+// MARK: - Inner Properties
+
+private extension HomeView {
+    var backgroundColors: (top: Color, bottom: Color) {
+        (.primaryNormal, Color(red: 248, green: 247, blue: 252))
+    }
+}
 
 // MARK: - Preview
 
