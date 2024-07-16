900121526c17d1b8834ca34aa671ac3a7994ca37
qwerty3345 <qwerty3345@naver.com>
2024-07-16 18:41:56 +0900

    [대황] 앱 기본 폴더링 구조 구성

---
+++ 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/App/PillyzeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillyzeCloneApp.swift
+//  PillyzeClone
+//
+//  Created by Mason Kim on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PillyzeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  PillyzeClone
+//
+//  Created by Mason Kim on 7/16/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
