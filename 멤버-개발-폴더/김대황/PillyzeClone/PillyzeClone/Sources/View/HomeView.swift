16c00e852df25525086261b0ce80778b42d11891
qwerty3345 <qwerty3345@naver.com>
2024-07-16 20:20:57 +0900

    [대황] 주간 날짜 선택 피커 구현, 홈뷰에 반영

--- 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/View/HomeView.swift
+++ 멤버-개발-폴더/김대황/PillyzeClone/PillyzeClone/Sources/View/HomeView.swift
@@ -8,10 +8,64 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedDate: Date = .now
+
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(spacing: 0) {
+                NavigationBar()
+                    .padding(.vertical, 8)
+
+                WeeklyDatePicker(
+                    selectedDate: $selectedDate,
+                    startRangeDate: .now.beforeDate(dayBefore: 100),
+                    endRangeDate: .now
+                )
+
+                Text("Hello, World!")
+            }
+        }
+        .background(.primaryNormal)
     }
 }
+
+// MARK: - Inner Views
+
+fileprivate struct NavigationBar: View {
+    var body: some View {
+        HStack {
+            Image(.logo)
+                .resizable()
+                .frame(width: 88, height: 23)
+
+            Spacer()
+
+            HStack(spacing: 8) {
+                Button {
+
+                } label: {
+                    Image(.icHeaderCalendar)
+                }
+
+                Button {
+
+                } label: {
+                    Image(.icHeaderAlarm)
+                }
+                
+                Button {
+
+                } label: {
+                    Image(.icHeaderUser)
+                }
+            }
+        }
+        .padding(.horizontal, 16)
+    }
+}
+
+
+// MARK: - Preview
 
 #Preview {
     HomeView()
