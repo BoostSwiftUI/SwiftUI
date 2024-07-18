e0331cdd63d9188600c8f5f856ad5bb2f42495f5
Xerath <yoondj98@naver.com>
2024-07-18 19:31:00 +0900

    [동주] init Pillyze project

---
+++ 멤버-개발-폴더/윤동주/Pillyze/Pillyze/Source/Application/PillyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillyzeApp.swift
+//  Pillyze
+//
+//  Created by 윤동주 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PillyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
