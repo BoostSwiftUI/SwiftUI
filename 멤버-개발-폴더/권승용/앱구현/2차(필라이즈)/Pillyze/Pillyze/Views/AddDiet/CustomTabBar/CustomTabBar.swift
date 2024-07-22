280717ad3f43c25b80dd0daff21e8a6915c4ade9
ericKwon95 <ericyong95@gmail.com>
2024-07-22 11:01:46 +0900

    [승용] feat: 탭바 탭 시 스크롤 이동 구현

--- 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/AddDiet/CustomTabBar/CustomTabBar.swift
+++ 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/AddDiet/CustomTabBar/CustomTabBar.swift
@@ -8,28 +8,70 @@
 import SwiftUI
 
 struct CustomTabBar: View {
+    @Namespace var frequentlyEatenID
+    @Namespace var favoritesID
+    @Namespace var directAddID
+    
+    @State private var indicatorOffset = CGFloat.zero
+    
     var body: some View {
         GeometryReader { proxy in
-            VStack {
-                HStack(spacing: 0) {
-                    CustomTab(title: "자주 드셨어요")
-                    CustomTab(title: "즐겨찾기")
-                    CustomTab(title: "직접 등록")
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        FrequentlyEaten(foods: ModelData().foods)
-                        
-                        Favorites(foods: ModelData().foods)
-                        
-                        DirectAdd(foods: ModelData().foods)
+            ScrollViewReader { scrollViewProxy in
+                VStack(spacing: 0) {
+                    VStack(spacing: 0) {
+                        HStack(spacing: 0) {
+                            CustomTab(title: "자주 드셨어요")
+                                .onTapGesture {
+                                    withAnimation {
+                                        scrollViewProxy.scrollTo(frequentlyEatenID)
+                                    }
+                                    indicatorOffset = 0
+                                }
+                            CustomTab(title: "즐겨찾기")
+                                .onTapGesture {
+                                    withAnimation {
+                                        scrollViewProxy.scrollTo(favoritesID)
+                                    }
+                                    indicatorOffset = proxy.size.width / 3
+                                }
+                            CustomTab(title: "직접 등록")
+                                .onTapGesture {
+                                    withAnimation {
+                                        scrollViewProxy.scrollTo(directAddID)
+                                    }
+                                    indicatorOffset = (proxy.size.width / 3) * 2
+                                }
+                        }
+                        Divider()
+                            .overlay {
+                                HStack {
+                                    Rectangle()
+                                        .foregroundStyle(.appPrimary)
+                                        .frame(width: proxy.size.width / 3, height: 2)
+                                        .offset(x: indicatorOffset, y: -1)
+                                        .animation(.default, value: indicatorOffset)
+                                    Spacer()
+                                }
+                            }
                     }
-                    .frame(width: proxy.size.width * 3)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            FrequentlyEaten(foods: ModelData().foods)
+                                .id(frequentlyEatenID)
+                            
+                            Favorites(foods: ModelData().foods)
+                                .id(favoritesID)
+                            
+                            DirectAdd(foods: ModelData().foods)
+                                .id(directAddID)
+                        }
+                        .frame(width: proxy.size.width * 3)
+                    }
+                    .scrollTargetBehavior(.paging)
                 }
-                .scrollTargetBehavior(.paging)
+                .ignoresSafeArea(edges: .bottom)
             }
-            .ignoresSafeArea(edges: .bottom)
         }
     }
 }
