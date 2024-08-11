fee3ed70ec84715c8974ff4958b7aa887dfb549b
Xerath <yoondj98@naver.com>
2024-08-12 03:27:22 +0900

    [동주] 기록 추가 버튼을 통한 Lottie 동작 구현

--- 멤버-개발-폴더/윤동주/Pillyze/Pillyze/Source/View/Record/FoodRankCell.swift
+++ 멤버-개발-폴더/윤동주/Pillyze/Pillyze/Source/View/Record/FoodRankCell.swift
@@ -6,38 +6,64 @@
 //
 
 import SwiftUI
+import DotLottie
 
 struct FoodRankCell: View {
+    
+    @Binding var selectedFoods: [RankingFood]
+    @Binding var isLottieAnimationShown: Bool
+    
     var food: RankingFood
     var index: Int
     
+    var isSelectedItem: Bool {
+        selectedFoods.contains(food)
+    }
+    
     var body: some View {
-        HStack {
-            Text("\(index)")
-                .font(.Pretendard.medium(.size16).font)
-            Image(food.rankChange == 0 ? "noChange" : food.rankChange > 0 ? "arrowUp" : "arrowDown")
-            VStack(alignment: .leading, spacing: 2) {
-                Text(food.name)
-                Text(food.unit)
-                    .foregroundStyle(.placeholderGray)
-            }
-            .font(.Pretendard.regular(.size16).font)
-            Spacer()
-            HStack(spacing: 12) {
-                Text("\(food.calory)Kcal")
+        ZStack {
+            HStack {
+                Text("\(index)")
                     .font(.Pretendard.medium(.size16).font)
-                    .foregroundStyle(.secondaryText)
-                Button {
-                    print("더하기 버튼 눌림")
-                } label: {
-                    Image("purpleAdd")
+                Image(food.rankChange == 0 ? "noChange" : food.rankChange > 0 ? "arrowUp" : "arrowDown")
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(food.name)
+                    Text(food.unit)
+                        .foregroundStyle(.placeholderGray)
+                }
+                .font(.Pretendard.regular(.size16).font)
+                Spacer()
+                HStack(spacing: 12) {
+                    Text("\(food.calory)Kcal")
+                        .font(.Pretendard.medium(.size16).font)
+                        .foregroundStyle(.secondaryText)
+                    Button {
+                        if let index = selectedFoods.firstIndex(of: food) {
+                            selectedFoods.remove(at: index)
+                        } else {
+                            selectedFoods.append(food)
+                        }
+                        withAnimation {
+                            if isSelectedItem {
+                                isLottieAnimationShown = true
+                            }
+                        }
+                    } label: {
+                        Image(isSelectedItem ? "purpleCheck" : "purpleAdd")
+                            .rotationEffect(.degrees(isSelectedItem ? 360 : 0))
+                            .animation(.easeInOut(duration: 0.4), value: isSelectedItem)
+                    }
+                    .sensoryFeedback(.increase, trigger: isSelectedItem)
                 }
             }
+            .padding(.vertical, 6)
         }
-        .padding(.vertical, 6)
     }
 }
 
 #Preview {
-    FoodRankCell(food: RankingFood.previewData, index: 4)
+    FoodRankCell(selectedFoods: .constant([]),
+                 isLottieAnimationShown: .constant(false),
+                 food: RankingFood.previewData,
+                 index: 4)
 }
--- 멤버-개발-폴더/윤동주/Pillyze/Pillyze/Source/View/Record/RecordButtonView.swift
+++ 멤버-개발-폴더/윤동주/Pillyze/Pillyze/Source/View/Record/RecordButtonView.swift
@@ -16,51 +16,76 @@ enum MealTime: String, CaseIterable {
 struct RecordButtonView: View {
     
     @State var mealTime: MealTime = .breakfast
+    @State private var shouldShowText: Bool = false
+    
+    @Binding var isLottieAnimationShown: Bool
+    @Binding var selectedFoods: [RankingFood]
     
     var body: some View {
-        HStack {
-            Menu {
-                ForEach(MealTime.allCases, id: \.self) { time in
-                    Button {
-                        self.mealTime = time
-                    } label: {
-                        Text("\(time.rawValue)")
+        VStack {
+            if shouldShowText {
+                Text("음식이 추가되었어요.")
+                    .font(.Pretendard.regular(.size16).font)
+            }
+            HStack {
+                Menu {
+                    ForEach(MealTime.allCases, id: \.self) { time in
+                        Button {
+                            self.mealTime = time
+                        } label: {
+                            Text("\(time.rawValue)")
+                        }
                     }
+                } label: {
+                    HStack {
+                        Text(mealTime.rawValue)
+                            .foregroundStyle(.primaryPurple)
+                            .font(.Pretendard.bold(.size20).font)
+                        Image(.arrowDown)
+                            .renderingMode(.template)
+                            .tint(.primaryPurple)
+                    }
+                    .padding(.horizontal, 14)
+                    .padding(.vertical, 8.5)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                    .background(.placeholderPurple)
                 }
-            } label: {
-                HStack {
-                    Text(mealTime.rawValue)
-                        .foregroundStyle(.primaryPurple)
-                    Image(.arrowDown)
-                        .renderingMode(.template)
-                        .tint(.primaryPurple)
+                
+                Text("에")
+                Button {
+                    print("기록하기 버튼 Clicked")
+                } label: {
+                    Text(selectedFoods.count > 0 ? "\(selectedFoods.count)개 기록하기" : "기록하기")
+                        .foregroundStyle(.white)
+                        .font(.Pretendard.regular(.size18).font)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 14.5)
+                        .background(selectedFoods.count > 0 ? .primaryPurple : Color(hex: "ECE8FF"))
                 }
-                .padding(.horizontal, 14)
-                .padding(.vertical, 8.5)
-                .clipShape(RoundedRectangle(cornerRadius: 4))
-                .background(.placeholderPurple)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding(.leading, 29)
+                
             }
-            
-            Text("에")
-            Button {
-                print("기록하기 버튼 Clicked")
-            } label: {
-                Text("기록하기")
-                    .foregroundStyle(.white)
-                    .font(.Pretendard.regular(.size18).font)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 14.5)
-                    .background(Color(hex: "ECE8FF"))
-            }
-            .clipShape(RoundedRectangle(cornerRadius: 8))
-            .padding(.leading, 29)
-            
         }
-        .frame(height: 67)
         .padding(.horizontal, 20)
+        .padding(.bottom, 8)
+        .onChange(of: isLottieAnimationShown) { _, newValue in
+            if newValue {
+                shouldShowText = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    shouldShowText = false
+                }
+            }
+        }
+        .animation(.easeInOut, value: shouldShowText)
     }
 }
 
 #Preview {
-    RecordButtonView()
+    RecordButtonView(isLottieAnimationShown: .constant(false),
+                     selectedFoods: .constant([RankingFood(name: "a",
+                                                           unit: "b",
+                                                           calory: 100,
+                                                           rank: 2,
+                                                           rankChange: -2)]))
 }
