ce70278f79dead05a91dec17fcbd5995422bd47e
ericKwon95 <ericyong95@gmail.com>
2024-08-19 21:38:28 +0900

    fix: 캘린더 컴포넌트 높이 및 마진 수정

--- 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/MyHealth/Header/Calender.swift
+++ 멤버-개발-폴더/권승용/앱구현/2차(필라이즈)/Pillyze/Pillyze/Views/MyHealth/Header/Calender.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-    struct Calender: View {
+struct Calender: View {
     let days = ["월", "화", "수", "목", "금", "토", "일"]
     
     var body: some View {
@@ -15,30 +15,41 @@ import SwiftUI
             HStack {
                 Spacer()
                 ForEach(0..<days.count, id: \.self) { index in
-                    Text(days[index])
-                        .font(.system(size: 12))
-                        .fontWeight(.regular)
-                        .foregroundStyle(.white)
-                        .opacity(0.5)
+                    calenderComponent(weekdays: days[index], daysInMonth: index + 10)
                     Spacer()
                 }
             }
             
-            HStack {
-                Spacer()
-                ForEach(0..<days.count, id: \.self) { index in
-                    Text("\(index)")
-                        .font(.system(size: 14))
-                        .fontWeight(.medium)
-                        .foregroundStyle(.white)
-                    Spacer()
-                }
-            }
         }
-        .padding(.vertical, 4)
         .background {
             Color(.appPrimary)
         }
+    }
+    
+    @ViewBuilder
+    func calenderComponent(weekdays: String, daysInMonth: Int ) -> some View {
+        VStack(spacing: 12) {
+            Text(weekdays)
+                .font(.system(size: 12))
+                .fontWeight(.regular)
+                .foregroundStyle(weekdays == "월" ? .appPrimary : .white)
+                .opacity(weekdays == "월" ? 1 : 0.5)
+                .frame(width: 24, height: 24)
+                .background {
+                    if weekdays == "월" {
+                        Circle()
+                            .fill(.white)
+                    }
+                }
+            
+            Text("\(daysInMonth)")
+                .font(.system(size: 14))
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .frame(width: 24, height: 24)
+        }
+        .padding(.top, 4)
+        .padding(.bottom, 16)
     }
 }
 
